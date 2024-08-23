2f1aaffd1525c9ff300ec7294112a68c3dfc342e
Umakani <umakani310@gmail.com>
2024-08-23 12:47:03 +0530

    .env file testing

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 
 // Access environment variables
-const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3002;
 
 // Sync database
 syncDatabase();
--- src/models/dbconfig.ts
+++ src/models/dbconfig.ts
@@ -1,11 +1,24 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
+import Messages from "./MessageModel";
+import Chats from "./ChatModel";
+import Files from "./FileModel";
 
 // Load environment variables from .env file
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+// Ensure that DATABASE_URL is defined
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "mssql://mssqldb:mssqldb@123@localhost:1433/chatapp";
+console.log("databaseUrl", databaseUrl);
+if (!databaseUrl || databaseUrl.trim() === "") {
+  throw new Error("DATABASE_URL is not set. Exiting...");
+}
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: "mssql",
+  // models: [Messages, Chats, Files],
   dialectOptions: {
     options: {
       encrypt: true, // Use this if you're on Windows Azure
